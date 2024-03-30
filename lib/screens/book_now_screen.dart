abb077437bbbe4536666095bf532c123c7b8b526
kravshan <pasidub23@gmail.com>
2024-03-30 09:36:36 +0530

    Book Now Screen developments

--- lib/screens/book_now_screen.dart
+++ lib/screens/book_now_screen.dart
@@ -15,8 +15,10 @@ class _BookNowScreenState extends State<BookNowScreen> {
     var height = MediaQuery.of(context).size.height;
     var containerWidth = width * 0.9;
 
-    final _fromController = TextEditingController();
-    final _toController = TextEditingController();
+    final fromController = TextEditingController();
+    final toController = TextEditingController();
+    final departureController = TextEditingController();
+    final returnController = TextEditingController();
 
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 22, 142, 84),
@@ -49,8 +51,93 @@ class _BookNowScreenState extends State<BookNowScreen> {
                       child: Column(
                         children: [
                           FromToWidget(
-                            fromController: _fromController,
-                            toController: _toController,
+                            fromController: fromController,
+                            toController: toController,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Departure Date'),
+                                    Container(
+                                      margin: const EdgeInsets.only(top: 8),
+                                      width: 240,
+                                      height: 40,
+                                      decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.black)),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 3, bottom: 5),
+                                            child: SizedBox(
+                                              width: 180,
+                                              child: TextField(
+                                                controller: departureController,
+                                              ),
+                                            ),
+                                          ),
+                                          IconButton(
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                                Icons.calendar_month),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(
+                                  width: 30,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Return Date'),
+                                    Container(
+                                      margin: const EdgeInsets.only(top: 8),
+                                      width: 240,
+                                      height: 40,
+                                      decoration: BoxDecoration(
+                                          border:
+                                              Border.all(color: Colors.black)),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                              left: 3,
+                                              bottom: 5,
+                                            ),
+                                            child: SizedBox(
+                                              width: 180,
+                                              child: TextField(
+                                                controller: returnController,
+                                              ),
+                                            ),
+                                          ),
+                                          IconButton(
+                                            onPressed: () {},
+                                            icon: const Icon(
+                                                Icons.calendar_month),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
                           )
                         ],
                       ),
